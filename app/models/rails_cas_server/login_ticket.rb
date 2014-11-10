004e6d115185fe92b5acaefaf79992f5da61625c
Greg Molnar <molnargerg@gmail.com>
2014-11-10 20:36:39 +0000

    login ticket must start with LT-

--- app/models/rails_cas_server/login_ticket.rb
+++ app/models/rails_cas_server/login_ticket.rb
@@ -2,7 +2,7 @@ class RailsCasServer::LoginTicket < ActiveRecord::Base
   before_save :generate_ticket
   scope :valid, ->(ticket) { where(ticket: ticket) }
   def generate_ticket
-    self.ticket = SecureRandom.uuid
+    self.ticket = "LT-#{SecureRandom.uuid}"
   end
 
   def to_s
